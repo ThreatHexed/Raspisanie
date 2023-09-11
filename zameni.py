dbb3178cbd9f5004b129a2c784dd175335f0ef66
1KAYYER1 <dima.evdokimov2003@yandex.ru>
2023-09-11 16:41:50 +0300

    modified:   excel_parser 	new file:   pari_norm.json 	new file:   raspisanie.doc 	new file:   raspisanie.docx 	modified:   zameni.py

--- zameni.py
+++ zameni.py
@@ -2,28 +2,34 @@ import json
 import re
 import openpyxl
 
-def zameni(day, group,):
+# def zameni(day, group,):
     
 
-# ///
-# Алллах защити мой код
-# ///
 
 
-    excel_file = openpyxl.load_workbook('X:/123/Output.xlsx')
-    employees_sheet = excel_file['Лист1']
+#     excel_file = openpyxl.load_workbook('X:/123/Output.xlsx')
+#     employees_sheet = excel_file['Лист1']
 
-with open('pari.json', 'r', encoding='utf-8') as f:
-    pari = json.load(f)
+# with open('pari.json', 'r', encoding='utf-8') as f:
+#     pari = json.load(f)
 
 
 
-    for p in range(0,7):
+#     for p in range(0,7):
     
-        for para in pari['pari'][p][group][day]:
-            for i in para:
-                cell = pari['pari'][p][group][day][i]['para']
-                pari_norm['pari'][p][group][day][i]['para'] = re.sub(" +", " ", str(employees_sheet[cell].value))
+#         for para in pari['pari'][p][group][day]:
+#             for i in para:
+#                 cell = pari['pari'][p][group][day][i]['para']
+#                 pari_norm['pari'][p][group][day][i]['para'] = re.sub(" +", " ", str(employees_sheet[cell].value))
 
 
-zameni()
+# zameni()
+
+s = "4п. – нет  5п. МДК 05.01 Рысцова, 412 ауд"
+
+
+b = (s.split('п.')[1])
+for i in (0,1):
+    for z in (0,1):
+        for x in list(re.sub(",", "", (s.split('п')[0+i]))):
+            print (x + s.split('п.')[1+z])
